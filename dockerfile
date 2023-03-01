1359f787848049e827b920fa3f48998a0453e0e7
fatfirebird <fatfirebird@gmail.com>
2023-03-01 18:38:19 +0400

    add docker, add db

--- dockerfile
+++ dockerfile
@@ -8,8 +8,4 @@ COPY package.json ./
 RUN yarn
 COPY . .
 
-ENV PORT 8080
-
-EXPOSE 8080
-
 CMD ["yarn", "start:dev"]
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
+const sequelize = new Sequelize('sqlite::memory:');
+
 const app = express();
 const port = process.env.PORT ?? 3000;
-console.log(process.env.NODE_ENV);
 
 app.get('/', (e, res) => {
   res.send('docker test');
 });
 
-app.listen(port, () => {
-  console.log('Example app listening on port ' + port);
-});
+const bootstrap = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    app.listen(port, () => {
+      console.log('Example app listening on port ' + port);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+bootstrap();

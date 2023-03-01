1501b6f4864ad1c221e66cfff6d71972eae2b81a
fatfirebird <fatfirebird@gmail.com>
2023-03-01 16:01:04 +0400

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT ?? 3000;
+console.log(process.env.NODE_ENV);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

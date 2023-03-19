0dbc12ed969dcd0e0bb02ababd7231a86c55501b
fatfirebird <fatfirebird@gmail.com>
2023-03-19 14:39:17 +0400

    add password hash

--- src/modules/user/user-service.ts
+++ src/modules/user/user-service.ts
@@ -1,12 +1,14 @@
 import { HttpError } from '@/core/error';
 import { User } from './user-entity';
 import { UserRepository } from './user-repository';
-
+import bcrypt from 'bcrypt';
 export class UserService {
   static async createUser(userData: User) {
     const error: HttpError | null = null;
 
-    const user = await UserRepository.createUser({ login: userData.login, passwordHash: 'fdsfdsfd' });
+    const hash = await bcrypt.hash(userData.password, 10);
+
+    const user = await UserRepository.createUser({ login: userData.login, passwordHash: hash });
 
     return { user, error };
   }
